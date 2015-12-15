8c7d68e69a4782f36d5f5a2fccbfd453857ea7cf
larouplus <larouplus@foxmail.com>
2015-12-15 15:24:03 +0800

    1.0

--- drag.js
+++ drag.js
@@ -2,14 +2,12 @@
 * @Author: anchen
 * @Date:   2015-12-15 14:25:16
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-12-15 15:15:04
+* @Last Modified time: 2015-12-15 15:23:29
 */
 
 'use strict';
 
 funciton drag(id){
     var obj = document.getElementById(id);
-    var x = 0;
-    var y = 0;
     obj.onmousedown = funciton(){}
 }
