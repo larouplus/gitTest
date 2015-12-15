b4ba4a72766fb393361ac2e5c9e07ae2c42a87ab
larouplus <larouplus@foxmail.com>
2015-12-15 15:49:15 +0800

    add test.txt

--- drag.js
+++ drag.js
@@ -2,12 +2,14 @@
 * @Author: anchen
 * @Date:   2015-12-15 14:25:16
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-12-15 15:23:29
+* @Last Modified time: 2015-12-15 15:28:47
 */
 
 'use strict';
 
 funciton drag(id){
     var obj = document.getElementById(id);
+    var x = 0;
+    var y = 0;
     obj.onmousedown = funciton(){}
 }
