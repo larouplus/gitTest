ef70243e886d5fc6e42ccc58e79e5954645b9f4b
larouplus <larouplus@foxmail.com>
2015-12-15 15:16:16 +0800

    最后的一个版本

--- drag.js
+++ drag.js
@@ -2,12 +2,14 @@
 * @Author: anchen
 * @Date:   2015-12-15 14:25:16
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-12-15 15:12:08
+* @Last Modified time: 2015-12-15 15:15:04
 */
 
 'use strict';
 
 funciton drag(id){
     var obj = document.getElementById(id);
+    var x = 0;
+    var y = 0;
     obj.onmousedown = funciton(){}
 }
