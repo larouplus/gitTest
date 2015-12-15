d26f0daad948b650305b902977fa2c09694d51db
larouplus <larouplus@foxmail.com>
2015-12-15 15:12:23 +0800

    1.0

--- drag.js
+++ drag.js
@@ -2,7 +2,7 @@
 * @Author: anchen
 * @Date:   2015-12-15 14:25:16
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-12-15 14:55:01
+* @Last Modified time: 2015-12-15 15:12:08
 */
 
 'use strict';
